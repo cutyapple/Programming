f96fce15b956aa1eb6f1aa611a9e88d1b7b04a0e
CutyApple <dbtldhs0915@naver.com>
2020-12-09 19:56:28 +0900

    백준 - 브론즈5 14652[JavaScript]

---
+++ 알고리즘/백준/브론즈5/14652.js
@@ -0,0 +1,6 @@
+// const fs = require('fs');
+// const number = fs.readFileSync('/dev/stdin').toString().split(" ").map(n => Number(n));
+
+const number = [3, 4, 6];
+
+console.log(parseInt(number[2] / number[1]), number[2] % number[1]);
