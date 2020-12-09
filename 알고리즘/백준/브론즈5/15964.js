0ebdb932733f2abbd9e81b58cdd271e99bc21722
CutyApple <dbtldhs0915@naver.com>
2020-12-09 19:57:05 +0900

    백준 - 브론즈5 15964 [JavaScript]

---
+++ 알고리즘/백준/브론즈5/15964.js
@@ -0,0 +1,6 @@
+// const fs = require('fs');
+// const numbers = fs.readFileSync('/dev/stdin').toString().split(" ").map(v => Number(v));
+
+const numbers = [4, 3];
+
+console.log((numbers[0] + numbers[1]) * (numbers[0] - numbers[1]));
