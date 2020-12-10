6e6e0adbaa7b1f60ddc17fc1742b24e2aa3efa4e
CutyApple <dbtldhs0915@naver.com>
2020-12-10 11:36:00 +0900

    백준 - 브론즈4 2525 [JavaScript]

---
+++ 알고리즘/백준/브론즈4/2525.js
@@ -0,0 +1,6 @@
+// const fs = require('fs');
+// const [nowArr, needNum] = fs.readFileSync('/dev/stdin').toString().split("\n");
+
+const [numArr, needNum] = ['14 30', '20'];
+
+console.log(numArr);
