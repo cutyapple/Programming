f80d113dac8337903552a739a19f3ba8e06ed44c
CutyApple <dbtldhs0915@naver.com>
2020-12-16 09:02:21 +0900

    백준 - 브론즈3 10992[JavaScript]

---
+++ 알고리즘/백준/브론즈3/10992.js
@@ -0,0 +1,24 @@
+// const fs = require('fs');
+// const n = Number(fs.readFileSync('/dev/stdin').toString())
+
+const n = 5;
+
+for (let i = 1; i <= n; i++) {
+	let text = '';
+	for (let j = 0; j < n - i; j++) {
+		text += ' ';
+	}
+	text += '*';
+	if (i !== 1 && i !== n) {
+		for (let j = 0; j < 2 * i - 3; j++) {
+			text += ' ';
+		}
+		text += '*';
+	}
+	if (i === n) {
+		for (let j = 0; j < 2 * n - 2; j++) {
+			text += '*';
+		}
+	}
+	console.log(text);
+}
